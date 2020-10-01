2b57c265dab423964138abca39b94138dcee193a
ravikumar532 <bhukyaravikumar532@gmail.com>
2020-10-01 11:11:39 +0530

    movement  jump  run of the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,28 @@ public class PlayerController : MonoBehaviour
 { 
     public Animator animator;
     public float speed;
+    public float jump;
+    private Rigidbody2D rbd;
     private BoxCollider2D Collider;
+    private void Awake() {
+        Debug.Log("Plyer get started");
+        rbd=gameObject.GetComponent<Rigidbody2D>();
+    }
      private void Update() {
         float horizontal=Input.GetAxisRaw("Horizontal");
-        MoveCharacter(horizontal);
-        PlayMovementAnimation(horizontal);
+        float vertical=Input.GetAxisRaw("Jump");
+        MoveCharacter(horizontal,vertical);
+        PlayMovementAnimation(horizontal,vertical);
     }
-    private void MoveCharacter(float horizontal){
+    private void MoveCharacter(float horizontal,float vertical){
         Vector3 position=transform.position;
         position.x+=horizontal * speed * Time.deltaTime;
         transform.position=position;
+        if(vertical>0){
+           rbd.AddForce(new Vector2(0f,jump),ForceMode2D.Force);
+        }
     }
-    private void PlayMovementAnimation(float horizontal){
+    private void PlayMovementAnimation(float horizontal,float vertical){
         animator.SetFloat("Speed",Mathf.Abs(horizontal));
         Vector3 scale=transform.localScale;
         if(horizontal<0){
@@ -26,5 +36,11 @@ public class PlayerController : MonoBehaviour
             scale.x=1f*Mathf.Abs(scale.x);
         } 
         transform.localScale=scale;
+        if(vertical>0){
+            animator.SetBool("Jump",true);
+        }
+        else{
+            animator.SetBool("Jump",false);
+        }
     }
 }
